61607712043bc717b7a865f29a6bbbcf0e7c791b
Henrique Ramos <henrique.s.ramos@outlook.com>
2023-12-27 20:33:42 -0300

    Desafio Completogit add .

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,12 +3,23 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        
+        // COMPLETO: TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
-        public Smartphone(string numero)
+        private string Modelo { get; set; } 
+
+        private string Imei { get; set; } 
+
+        private int Memoria { get; set; } 
+        
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            // COMPLETO: TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = Modelo;
+            Imei= imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,25 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+class Program
+
+{
+    static void Main (string[] args)
+    {
+
+        Iphone ip1 = new Iphone("12345678", "Não tenho Iphone", "987654321", 128);
+
+        ip1.InstalarAplicativo("Itune");
+        ip1.ReceberLigacao();
+        ip1.Ligar();
+
+        Nokia no1 = new Nokia("40028922", "Tijolão", "666000234", 256);
+
+        no1.InstalarAplicativo("Free fire");
+        no1.ReceberLigacao();
+        no1.Ligar();
+    }
+}
+
+
